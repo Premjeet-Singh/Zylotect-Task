7bf45d148f59e14409547ab7e99237a77858bf84
Premjeet-Singh <itsprem2193@gmail.com>
2017-07-27 17:07:34 +0530

    commit

---
+++ routes/index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// router.get('/test', function(req, res, next) {
+//   res.send('hello test');
+// });
+
+router.get('/task2', function(req, res, next) {
+
+var fs = require('fs')
+  , filename = process.argv[2];
+
+fs.readFile('test.txt', 'utf8', function(err, data) {
+  if (err) throw err;
+  // console.log('OK: ' + test);
+  console.log("BEFORE REMOVING SPECIAL CHARACTERS:")
+  console.log(data)
+  var str = data;
+  console.log("AFTER REMOVING SPECIAL CHARACTERS:")
+	console.log(str.replace(/[^a-zA-Z. ]/g, ""));
+  res.send('See console for result');
+  
+});
+
+});
+
+
+router.get('/task1', function(req, res, next) {
+	xyloHack(81)
+  res.send('See console for result');
+});
+
+module.exports = router;
+
+
+function xyloHack(n) {
+		var str = 'i love JavaScript';
+
+		if(n%2==0){
+			console.log("Number "+n +" is even");
+			console.log(str.toUpperCase())
+			
+		} else {
+			console.log("Number "+n +" is odd");
+			console.log(str.toLowerCase())
+		}
+	 // console.log("hello: ", n);  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// xyloHack(81)
+
+	// var dot = 'fooBar.md';
+	// var doll = 'da$$rk lord \n accounta$ble';
+	// var doll2 = 'desm@ond is god';
+	// var doll3 = 'accounta$ble';
